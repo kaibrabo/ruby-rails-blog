04f30c737bd457aac8866f7c4b6eb3de13701911
Kainoa Brabo <kaibrabo@gmail.com>
2019-07-24 12:48:37 -0700

    add voting to posts

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,13 @@
 class Vote < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  after_save :update_post
 
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }, presence: true
+
+  private
+  
+  def update_post
+    post.update_rank
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -56,16 +56,21 @@ puts "#{Topic.count} topics created"
 
 # Creates 50 random posts
 50.times do
-    Post.create!(
+    post = Post.create!(
         user:   users.sample,
         topic:  topics.sample,
         title:  RandomData.random_sentence,
         body:   RandomData.random_paragraph
     )
+    
+    post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+    rand(1..5).times { post.votes.create!(value: [-1,1].sample, user: users.sample) }
 end
 
 # sets all post objects to 'posts' 
 posts = Post.all    
+
+puts "#{Vote.count} votes created"
 puts "#{Post.count} posts created"
 
 # Creates 100 comments
